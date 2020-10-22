2c72f53e74515f6d3835e6588904f9e7f0d2b0c7
BMarcin <m1.borzymowski@gmail.com>
2020-10-22 23:09:08 +0200

    documentation test

---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. MordinezNLP documentation master file, created by
+   sphinx-quickstart on Sat Oct 17 22:47:32 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to MordinezNLP's documentation!
+=======================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   utils
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- src/MordinezNLP/utils/ngram_iterator.py
+++ src/MordinezNLP/utils/ngram_iterator.py
@@ -1,17 +1,22 @@
-def ngram_iterator(string: str, ngram: int = 3):
+def ngram_iterator(string: str, ngram_len: int = 3) -> list:
     """
     Iterates over string creating list of strings.
+    For example for string "hello" and ngram_len set to 3 it will output
+    ["hel", "ell", "llo"]
 
-    :param item:
-    :param ngram:
-    :return:
+    Args:
+        string: string to iterate on
+        ngram_len: lenght of each ngram
+
+    Returns:
+        ngram - list of ngram_len characters of input string
     """
-    if len(string) < ngram:
+    if len(string) < ngram_len:
         return [string]
 
     to_return = []
-    for i in range(0, len(string) - ngram + 1):
-        to_return.append(string[i:i + ngram])
+    for i in range(0, len(string) - ngram_len + 1):
+        to_return.append(string[i:i + ngram_len])
     return to_return
 
 
