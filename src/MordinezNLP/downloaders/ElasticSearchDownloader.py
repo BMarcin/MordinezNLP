243103e374d8a78b9be064ddb17b56d0a97d1969
BMarcin <m1.borzymowski@gmail.com>
2020-12-26 16:19:37 +0100

    add ElasticSearchDownloader.py file

--- src/MordinezNLP/downloaders/ElasticSearchDownloader.py
+++ src/MordinezNLP/downloaders/ElasticSearchDownloader.py
@@ -1,208 +1,177 @@
-from multiprocessing import Pool
-from typing import List, Iterable, Callable, Any
+***REMOVED***
+***REMOVED***
 
-from elasticsearch import Elasticsearch
-
-try:
-    from src.MordinezNLP.parsers import HTML_Parser
-except:
-    from ..parsers import HTML_Parser
+***REMOVED***
 
 
-class ElasticSearchDownloader:
-    """
-    Class used to download elastic search data from specified index using multithreading
-    """
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
 
-    def __init__(self, ip: str, port: int, timeout: int = 100):
-        """
-        Save elastic search connection data to class variables.
+***REMOVED***
+    ***REMOVED***
+***REMOVED***
 
-        Args:
-            ip (str): elastic search IP
-            port (int): elastic search port
-            timeout (int): elastic search connection timeout
-        """
-        self.ip: str = ip
-        self.port: int = port
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+    ***REMOVED***
+***REMOVED***
+***REMOVED***
 
-        self.client = ElasticSearchDownloader._get_es_connection(
-            ip=ip,
-            port=port,
-            timeout=timeout
-        )
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
 
-    @staticmethod
-    def _get_es_connection(ip: str, port: int, timeout: int) -> Elasticsearch:
-        """
-        Build elastic search connection.
+***REMOVED***
+***REMOVED***
+    ***REMOVED***
+***REMOVED***
 
-        Args:
-            ip (str): elastic search IP
-            port (int): elastic search port
-            timeout (int): elastic search connection timeout
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
 
-        Returns:
-            Elasticsearch: elasti search client connection
-        """
-        return Elasticsearch(
-            [
-                {
-                    'host': ip,
-                    'port': port
-                }
-            ],
-            timeout=timeout
-        )
+***REMOVED***
+***REMOVED***
+    ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
 
-    def get_all_available_indexes(self) -> List[str]:
-        """
-        Get all available elastic search indexes.
+***REMOVED***
+    ***REMOVED***
+***REMOVED***
 
-        Returns:
-            List[str]: a list of string where each element is a elastic search index name
-        """
-        return [
-            index for index in self.client.indices.get_alias().keys()
-        ]
+***REMOVED***
+***REMOVED***
+    ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
 
-    @staticmethod
-    def _multithreaded_processing(
-            processing_function: Callable[[dict], Any],
-            args: Iterable,
-            threads: int = 6
-    ) -> List[any]:
-        """
-        A threading wrapper for processing list
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+    ***REMOVED***
+***REMOVED***
 
-        Args:
-            processing_function (Callable[[dict], Any]): A function which will be used to process single document downloaded from elastic search
-            args (Iterable): Arguments redirected to the *processing_function*
-            threads (int): Number of threads to process documents
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
 
-        Returns:
-            Any: Returns a processed item according to a return type and item from *processing_function*
-        """
-        with Pool(threads) as p:
-            return p.starmap(
-                processing_function,
-                args
-            )
+***REMOVED***
+***REMOVED***
+    ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+    ***REMOVED***
 
-    def scroll_data(
-            self,
-            index_name: str,
-            query: dict,
-            processing_function: Callable[[dict], Any],
-            threads: int = 6,
-            scroll: str = '2m',
-            scroll_size: int = 100
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***,
+***REMOVED***
+***REMOVED***
 
-    ) -> List[any]:
-        """
-        A function that scrolls through an elastic search data from index and returns a multithreaded data processed with *processing_function*.
-        It returns a List of types returned by a *processing_function*.
+***REMOVED***
+    ***REMOVED***
+***REMOVED***
+***REMOVED***
 
-        Args:
-            index_name (str): An index name to scroll/download the data
-            query (dict): An elastic search query
-            processing_function (Callable[[dict], Any]): A function that processes single item from elastic search index
-            threads (int): A number of threads to run processing on
-            scroll (str): A scroll value
-            scroll_size (int): A size of scrolling items at once
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
 
-        Returns:
-            List[any]: Returns a list of processed items with type according to a *processing_function* or empty list if index doesn't exists.
-        """
-        if self.client.indices.exists(index_name):
-            processed_docs = []
+***REMOVED***
+***REMOVED***
+    ***REMOVED***
+***REMOVED***
+***REMOVED***
 
-            data = self.client.search(
-                index=index_name,
-                scroll=scroll,
-                size=scroll_size,
-                body=query
-            )
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+    ***REMOVED***
 
-            processed_docs += ElasticSearchDownloader._multithreaded_processing(
-                processing_function=processing_function,
-                args=zip(
-                    data['hits']['hits']
-                ),
-                threads=threads
-            )
+***REMOVED***
+***REMOVED***
+***REMOVED***=zip(
+***REMOVED***
+        ***REMOVED***,
+***REMOVED***
+    ***REMOVED***
 
-            scroll_id = data['_scroll_id']
-            scroll_size = len(data['hits']['hits'])
+***REMOVED***
+***REMOVED***
 
-            while scroll_size > 0:
-                data = self.client.scroll(
-                    scroll_id=scroll_id,
-                    scroll=scroll
-                )
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+        ***REMOVED***
 
-                processed_docs += ElasticSearchDownloader._multithreaded_processing(
-                    processing_function=processing_function,
-                    args=zip(
-                        data['hits']['hits']
-                    ),
-                    threads=threads
-                )
+    ***REMOVED***
+    ***REMOVED***
+    ***REMOVED***=zip(
+    ***REMOVED***
+            ***REMOVED***,
+    ***REMOVED***
+        ***REMOVED***
 
-                scroll_id = data['_scroll_id']
-                scroll_size = len(data['hits']['hits'])
-            return processed_docs
-        else:
-            print('{} doesn\'t exists'.format(index_name))
-            return []
-
-
-if __name__ == '__main__':
-    es = ElasticSearchDownloader(
-        ip='51.210.123.137',
-        port=9200,
-        timeout=10
-    )
-
-    # print(es.get_all_available_indexes())
-    for index in es.get_all_available_indexes():
-        if 'ep' in index or 'ukp' in index:
-            print(index)
-
-    body = {
-            "query": {
-                "bool": {
-                    "must": [
-                        {
-                            "match": {
-                                "language_version_info.en.keyword": "original"
-                            }
-                        }
-                    ]
-                }
-            }
-        }
-
-    ' Your own processing function for a single element '
-    def processing_func(data: dict) -> str:
-        if 'en' in data['_source']:
-            if 'content' in data['_source']['en']:
-                content = " ".join(part['content'] for part in data['_source']['en']['content'])
-                parsed = HTML_Parser(content, separator='... \n')
-                print(parsed)
-                return parsed
-        return ""
+    ***REMOVED***
+    ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+    ***REMOVED***]
 
 
-    ' Scroll the data '
-    downloaded_elastic_search_data = es.scroll_data(
-        'ukp_commons_debates',
-        body,
-        processing_func,
-        threads=8,
-        scroll_size=300
-    )
+***REMOVED***
+***REMOVED***
+        ip='',
+***REMOVED***
+***REMOVED***
+***REMOVED***
 
-    print(len(downloaded_elastic_search_data))
+***REMOVED***}  # <- use your own elastic search query
 
-    print(downloaded_elastic_search_data[:3])
+***REMOVED***
+***REMOVED***
+        return data['my_key']['my_next_key'].replace("\r\n", "\n")
+
+
+***REMOVED***
+***REMOVED***
+        'my_index_name',
+***REMOVED***
+***REMOVED***
+        threads=8
+***REMOVED***
+
+***REMOVED***
